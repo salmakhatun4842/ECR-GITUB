5534661699af1424f23b1c8e1b0865abe2aa3d02
salmakhatun1514 <53810988+salmakhatun4842@users.noreply.github.com>
2024-10-18 11:30:14 +0530

    Update ecr_image.py

--- ecr_image.py
+++ ecr_image.py
@@ -11,19 +11,17 @@ images = client.describe_images(repositoryName=repository_name)['imageDetails']
 latest_image = max(images, key=lambda x: x['imagePushedAt'])
 
 # Get a list of image digests that are not the latest
-non_latest_images = [image for image in images if image != latest_image]
+non_latest_images = [image['imageDigest'] for image in images if image != latest_image]
 
-# Delete non-latest images and prepare Markdown table
-# markdown_table = "| Image Digest | Pushed At |\n|--------------|-----------|\n"
-# if non_latest_images:
-#     response = client.batch_delete_image(
-#         repositoryName=testing,
-#         imageIds=[{'imageDigest': image['imageDigest']} for image in non_latest_images]
-#     )
-#     for image in non_latest_images:
-#         markdown_table += f"| {image['imageDigest']} | {image['imagePushedAt']} |\n"
-#     print(f"Deleted {len(response['imageIds'])} images.")
-# else:
+# Delete non-latest images
+if non_latest_images:
+    response = client.batch_delete_image(
+        repositoryName=testing,
+        imageIds=[{'imageDigest': digest} for digest in non_latest_images]
+    )
+    print(f"Deleted {len(response['imageIds'])} images.")
+else:
+    print("No images to delete."
 #     print("No images to delete.")
 
 # print("Markdown Table:\n")
